99a216be196c86d00963dfa115ef0a70019920f6
Om Prakash Das <omkletu+github@gmail.com>
2024-12-07 17:11:59 +0530

    polls model

---
+++ src/models/poll.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const optionSchema = new mongoose.Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+  text: { type: String, required: true },
+});
+
+const pollSchema = new mongoose.Schema({
+  question: { type: String, required: true },
+  options: [optionSchema],
+  votes: [
+    {
+      user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      optionId: { type: mongoose.Schema.Types.ObjectId },
+    },
+  ],
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("Poll", pollSchema);
